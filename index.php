ea7d49940a2cc03b1eeeb736c0ab9b3f441219ea
Your Name <you@example.com>
2025-01-17 13:56:18 +0100

    Vytvorenie formulara

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Registrácia</h3>
+
+    <form action="index.php" method="post">
+        <input type="text" name="login" placeholder="Zadajte login" required> <br>
+        <input type="password" name="heslo" placeholder="Zadajte heslo" required> <br>
+        <input type="submit" name="tlacidlo" value="Prihlasiť sa">
+    </form>
+
+</body>
+</html>
