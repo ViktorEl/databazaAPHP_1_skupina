8ec0008956a172e2caf2013ef023d3b9c227f34c
Your Name <you@example.com>
2025-02-28 13:01:54 +0100

    doplnenie required do dotaznika

--- index2.php
+++ index2.php
@@ -8,20 +8,20 @@
     <h2>Prosíme vás o vyplnenie dotaznika</h2>
     <form action="index2.php" method="post">
         <h3>Ohodnotte prostrestredie reštauracie od 1 do 3</h3>
-        <input type="radio" name="prostredie" value="1"><label>Výborne</label> <br>
-        <input type="radio" name="prostredie" value="2"><label>Dobré</label> <br>
-        <input type="radio" name="prostredie" value="3"><label>Zlé</label> <br>
+        <input type="radio" name="prostredie" value="1" required><label>Výborne</label> <br>
+        <input type="radio" name="prostredie" value="2" required><label>Dobré</label> <br>
+        <input type="radio" name="prostredie" value="3" required><label>Zlé</label> <br>
 
         <h3>Ohodnotte chuť pripraveného jedla</h3>
-        <input type="radio" name="jedlo" value="1"><label>Výborne</label> <br>
-        <input type="radio" name="jedlo" value="2"><label>Dobré</label> <br>
-        <input type="radio" name="jedlo" value="3"><label>Zlé</label> <br>
+        <input type="radio" name="jedlo" value="1" required><label>Výborne</label> <br>
+        <input type="radio" name="jedlo" value="2" required><label>Dobré</label> <br>
+        <input type="radio" name="jedlo" value="3" required><label>Zlé</label> <br>
 
         <h3>Napíšte oblúbene jedlo</h3>
-        <input type="text" name="chutovka" placeholder="Napíšte oblubene jedlo"><br>
+        <input type="text" name="chutovka" placeholder="Napíšte oblubene jedlo" required><br>
 
         <h3>Celková spokojnosť reštauracie</h3>
-        <input type="number" name="hodnotenie" placeholder="Zadajte celkovu spokojnost od 1 do 5" min="1" max="5"><br>
+        <input type="number" name="hodnotenie" placeholder="Zadajte celkovu spokojnost od 1 do 5" min="1" max="5" required><br>
 
         <input type="submit" name="tlacidlo" value="Odoslať">
 
