8759ed1b24e000103d1f390fc1d2552f677e71bf
Your Name <you@example.com>
2025-03-14 13:19:40 +0100

    vypis z db pomocou tabulky nedokoncene

--- vypis.php
+++ vypis.php
@@ -20,24 +20,36 @@
             $nazov_db = "restauracia";
             
             $pripojenie = mysqli_connect($server, $meno, $heslo, $nazov_db);
-
+            mysqli_set_charset($pripojenie, 'utf8');
             $dotaz = "SELECT * FROM hodnotenie";
 
             $vysledok = mysqli_query($pripojenie, $dotaz);
 
-            echo "id prostredie jedlo chutovka hodnotenie";
-            echo "<br>";
-            while($riadok = mysqli_fetch_assoc($vysledok)) {
-                echo $riadok["id"]." ".$riadok["prostredie"]." ".$riadok["jedlo"]." ".$riadok["chutovka"]." ".$riadok["hodnotenie"];
-                echo "<br>";
+            echo "<table>";
             
+            echo "<tr>
+                    <th>id</th>
+                    <th>prostredie</th>
+                    <th>jedlo</th>
+                    <th>chutovka</th>
+                    <th>hodnotenie</th>
+                </tr>";
+            while($riadok = mysqli_fetch_assoc($vysledok)) {
+                echo "<tr>";
+                    echo "<td>".$riadok["id"]."</td>";
+                    echo "<td>".$riadok["prostredie"]."</td>";
+                    echo "<td>".$riadok["jedlo"]."</td>";
+                    echo "<td>".$riadok["chutovka"]."</td>";
+                    echo "<td>".$riadok["hodnotenie"]."</td>";
+                echo "</tr>";
             }
-
-
-
-
-
+            echo "</table>";
         }
+
+
+
+
+
 
 
 
